82841e60f59fdb95c74559b6ae989c68990579a0
Ashley Knorr Gehring <ashleyknorr@gmail.com>
2021-04-11 21:26:07 -0500

    commit tree df8b37716d78b0631072e254aa47c2d166c4abf9

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+const driverNames = drivers.filter( findMatching => findMatching.name === 'Sally');
